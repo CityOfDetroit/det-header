a395d70ce32d7703cd75a379a33e504fef079d45
Michael Evans <magic6435@gmail.com>
2019-02-26 13:26:52 -0500

    usestate examples

--- src/App.js
+++ src/App.js
@@ -1,22 +1,50 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
-import Logo from './Logo.js';
-import Search from './Search.js';
-import Menu from './Menu.js';
+import React, { Component, useState } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import Logo from "./Logo.js";
+import Search from "./Search.js";
+import Menu from "./Menu.js";
+import BigMenu from "./BigMenu";
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <Logo title="mikes button" type="primary"/>
-          <Menu/>
-          <Search/>
-        </header>
-      </div>
-    );
-  }
+function App() {
+  const [menu, setMenu] = useState(false);
+  const [buses, setBuses] = useState(false);
+
+  const toggleBuses = event => {
+    console.log("how do it");
+    console.log(event.target);
+    setBuses(menuState => !menuState);
+  };
+  const toggleMenu = () => {
+    setMenu(menuState => !menuState);
+  };
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        <button onClick={toggleMenu}>open Menu</button>
+        <Logo title="mikes button" type="primary" />
+        <Menu />
+        <Search />
+        <BigMenu display={menu} buses={buses} setBuses={toggleBuses} />
+        {/* {menu && } */}
+      </header>
+    </div>
+  );
 }
+
+// class App extends Component {
+//   render() {
+//     return (
+//       <div className="App">
+//         <header className="App-header">
+//           <Logo title="mikes button" type="primary"/>
+//           <Menu/>
+//           <Search/>
+//         </header>
+//       </div>
+//     );
+//   }
+// }
 
 export default App;
